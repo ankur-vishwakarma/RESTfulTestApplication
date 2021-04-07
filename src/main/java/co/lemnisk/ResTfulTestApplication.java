c2eaeb6d0c1bb79af47d1b868401cbebd253a884
Ankur Vishwakarma <ankur.vishwakarma@lemnisk.co>
2021-04-07 21:30:50 +0530

    setting remote

---
+++ src/main/java/co/lemnisk/ResTfulTestApplication.java
@@ -0,0 +1,14 @@
+package co.lemnisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulTestApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
